b15edae59dfbd363de92665726110c42009711d8
frontenddev344 <frontenddev344@gmail.com>
2025-07-22 18:45:42 +0530

    init

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".menu-toggle").click(function(){
+      $("body").addClass("toggle");
+      $(".closed-menu").click(function(){
+        $("body").removeClass("toggle");
+      });
+    });
+
+
+  });
