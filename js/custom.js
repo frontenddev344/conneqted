8cb9e27b95409b26adcc8aab26213cfade37e199
frontenddev344 <frontenddev344@gmail.com>
2025-08-06 17:53:01 +0530

    update homepage

--- js/custom.js
+++ js/custom.js
@@ -24,4 +24,56 @@ $(document).ready(function(){
 
   });
 
-  
+$(document).ready(function(){
+$(".acc-head").click(function(){
+  $(this).next().slideToggle().parent().siblings().find(".acc-content").slideUp();
+  $(this).toggleClass("active").parent().siblings().find(".acc-head").removeClass("active");
+});
+});
+
+
+
+// Add animations to the timeline
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".stack-scroll-container",
+//     start: "top top",
+//     end: "bottom center",
+//     scrub: 1,
+//     pin: true,
+//     pinSpacing: true,
+//     // markers: true
+//   }
+// });
+
+// tl.to(".stack-card", {
+//   yPercent: -100,
+
+// })
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.querySelectorAll(".stack-scroll-container").forEach((section) => {
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: section,
+      start: "top top",
+      end: "bottom center",
+      scrub: 1,
+      pin: true,
+      pinSpacing: true,
+      // markers: true
+    }
+  })
+  .to(section.querySelector(".stack-card"), {
+    yPercent: -100,
+    ease: "none"
+  });
+});
+
+var typing=new Typed(".reveal-text", {
+  strings: ["", "efforless living", "one system", "total control"],
+  typeSpeed: 100,
+  backSpeed: 40,
+  loop: true,
+});
